f4a4c47ec0ab64aa353f5330aa49178b25afbd9d
bioinfoguru <support@bioinfo.guru>
2022-06-17 16:10:18 +0530

    New

---
+++ _build/jupyter_execute/Covid19.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+df1 = pd.read_csv("owid-covid-data.csv")
+
+
+# In[3]:
+
+
+df1.head()
+
+
+# ### Exercise
+# Find top 10 countries having highest number of total Covid cases till date.
+
+# In[221]:
+
+
+top_10 = df1.groupby(["location"], as_index=False)["new_cases"].sum().sort_values(by="new_cases", ascending=False)[8:18]
+
+
+# In[222]:
+
+
+top_10
+
+
+# Using `nlargest()` function
+
+# In[223]:
+
+
+df1.groupby(["location"],as_index=False)["new_cases"].sum().nlargest(10,"new_cases")
+
+
+# Selecting data for a particular date.
+
+# In[241]:
+
+
+df1[df1["date"]=="2022-06-13"].nlargest(10,"total_cases")#[["location","total_cases"]]
+
+
+# In[33]:
+
+
+top_10_r
+
+
+# In[437]:
+
+
+fig, ax = plt.subplots()
+ax.barh(top_10_r.index,top_10_r.values, color="lightblue")
+ax.spines[["top","right","left"]].set_visible(False)
+ax.yaxis.set_ticks_position('none')
+a=ax.get_yticklabels()
+ax.grid('on',which='major',axis='x')
+
+plt.show()
+
+
+# ### Changing axis scale
+# 
+
+# In[145]:
+
+
+import numpy as np
+
+
+# In[242]:
+
+
+#Exponential 
+N=1
+x = range(1,11)
+y = []
+for a in x:
+    N = N*2
+    y.append(N)
+
+fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2,figsize=(9,3))
+plt.rcParams.update({'font.size': 14})
+
+ax1.plot(x,y)
+ax2.plot(x,y)
+ax2.set_yscale('log')
+
+ax1.set_title("Linear Scale")
+ax2.set_title("Log Scale")
+
+
+plt.show()
+
+
+# In[298]:
+
+
+fig,ax=plt.subplots(figsize=(8,3))
+df1[df1["location"]=="India"].plot(x="date", y="total_cases",ax=ax)
+df1[df1["location"]=="United States"].plot(x="date", y="total_cases",ax=ax)
+df1[df1["location"]=="France"].plot(x="date", y="total_cases",ax=ax)
+plt.legend(["India","United States", "France"])
+plt.ylabel("Total Cases")
+#plt.yscale("log")
+plt.show()
+
+
+# ### Function to plot total cases
+
+# In[267]:
+
+
+def plot_country(*args):
+    fig,ax=plt.subplots(figsize=(8,3))
+    for c in args:
+        df1[df1["location"]==c].plot(x="date", y="total_cases",ax=ax)
+    plt.legend(args)
+    plt.show()
+
+plot_country("India","United States")
+
+
+# In[436]:
+
+
+fig,ax=plt.subplots(figsize=(8,3))
+df1[df1["location"]=="India"].plot(x="date", y="new_cases",ax=ax)
+#df1[df1["location"]=="United States"].plot(x="date", y="new_cases",ax=ax)
+plt.show()
+
+
+# In[349]:
+
+
+df_India = df1[df1["location"]=="India"]
+
+
+# In[353]:
+
+
+df_India[(df_India["date"] >= '2022-01-01') & (df_India["date"] <= '2022-01-31')].head()
+
+
+# In[354]:
+
+
+df_India.dtypes
+
+
+# In[355]:
+
+
+df_India["date"] = pd.to_datetime(df_India['date'])
+
+
+# In[385]:
+
+
+df_India_2022 = df_India[(df_India["date"] >= '2022-01-01')]
+
+
+# In[386]:
+
+
+df_months = df_India_2022.groupby(df_India_2022['date'].dt.strftime('%B'))["new_cases"].sum().to_frame().reset_index()#.plot(kind="bar")
+
+
+# In[387]:
+
+
+print(df_months.dtypes)
+df_months.sort_values(by="date",inplace=True, ignore_index=True)
+df_months
+
+
+# In[388]:
+
+
+months = ["January", "February", "March", "April", "May", "June", 
+          "July", "August", "September", "October", "November", "December"]
+df_months['date'] = pd.Categorical(df_months['date'], categories=months, ordered=True)
+df_months.sort_values(by="date",inplace=True, ignore_index=True)
+print(df_months.dtypes)
+df_months
+
+
+# In[401]:
+
+
+fig,ax=plt.subplots()
+df_months.plot(kind="bar",xlabel="date", ax=ax)
+plt.show()
+
+
+# In[407]:
+
+
+df_India_2022["date"] = df_India_2022["date"].dt.strftime('%B')
+
+
+# In[431]:
+
+
+months = ["January", "February", "March", "April", "May", "June"]
+df_India_2022['date'] = pd.Categorical(df_India_2022['date'], categories=months, ordered=True)
+df_India_2022.sort_values(by="date",inplace=True, ignore_index=True)
+df_India_2022.dtypes
+
+
+# In[434]:
+
+
+ax = df_India_2022.boxplot(column="new_cases",by="date")
+plt.title("")
+plt.show()
+
--- _build/jupyter_execute/Matplotlib_Intro.py
+++ _build/jupyter_execute/Matplotlib_Intro.py
@@ -4,21 +4,24 @@
 # ## Introduction to Matplotlib
 # A Python library for data visualization. It offers variety of functions to plot different types of graphs which can be customised to create publication quality figures. The `pyplot` function in this library is used for instantiating a matplotlib graph object.
 
-# In[2]:
+# In[1]:
 
 
 import matplotlib.pyplot as plt
 
 
-# In[37]:
+# In[4]:
 
 
 x = range(1,11)
 y = [a**2 for a in x]
+
 plt.plot(x,y) #line plot
+
 plt.scatter(x,y) #scatter plot
 plt.xlabel("Number")
 plt.ylabel("Square")
+
 plt.show()
 
 
@@ -33,7 +36,7 @@ plt.ylabel("Square of the Number", fontsize=12)
 plt.show()
 
 
-# In[79]:
+# In[5]:
 
 
 x = range(1,11)
@@ -52,7 +55,7 @@ plt.show()
 # ### Subplots
 # The `pyplot` class has `subplot()` function that return a figure and and axes object. These can be used to access and manipulated different elements of the graph. In addition, subplots can take number of plots as argument to create a figure with multiple plots. This function has a keyword argument `figsize` to specify the size of the plot.
 
-# In[5]:
+# In[6]:
 
 
 x = range(1,11)
@@ -60,24 +63,28 @@ y = [a**2 for a in x]
 z = [a**3 for a in x]
 
 fig, ax = plt.subplots()
+
 ax.scatter(x,y,marker="^", color="red")
 ax.scatter(x,z,marker="*", color="purple")
 
 plt.show()
 
 
-# In[36]:
+# In[15]:
 
 
 x = range(1,11)
 y = [a**2 for a in x]
 z = [a**3 for a in x]
 
-fig, ax = plt.subplots(2,1) #create subplots with two rows and one column
+fig, ax = plt.subplots(2,1,sharex=True) #create subplots with two rows and one column
+
+
+
 ax[0].scatter(x,y,marker="^", color="red")
 ax[1].scatter(x,z,marker="*", color="purple")
 
-ax[0].set_xlabel("Number")
+#ax[0].set_xlabel("Number")
 ax[1].set_xlabel("Number")
 
 ax[0].set_ylabel("Squares")
@@ -87,7 +94,7 @@ ax[1].set_ylabel("Cubes")
 plt.show()
 
 
-# In[43]:
+# In[69]:
 
 
 x = range(1,11)
@@ -97,9 +104,35 @@ z = [a**3 for a in x]
 fig, ax = plt.subplots(2,2) #create subplots with two rows and two columns
 ax[0,0].scatter(x,y,marker="^", color="red")
 ax[1,0].scatter(x,z,marker="*", color="purple")
+ax[1,0].bar(x,z,color="purple",alpha=0.1)
+
 ax[0,1].bar(x,y,color="red")
 ax[1,1].bar(x,z,color="purple")
 
 
+fig.savefig("subplot.png", dpi=300)
+plt.show()
+
+
+# ### plt.subplot (without 's')
+# The `subplot()` function is similar to subplots with a difference that it take an additional argument - index. This can be used to make axes span multiple columns within the subplot. 
+
+# In[96]:
+
+
+x = range(1,11)
+y = [a**2 for a in x]
+z = [a**3 for a in x]
+
+ax1 = plt.subplot(2,1,1) 
+ax2 = plt.subplot(2,2,3)
+ax3 = plt.subplot(2,2,4)
+
+ax1.scatter(x,y,marker="^", color="red")
+ax1.bar(x,y, color="red",alpha=0.1)
+ax2.scatter(x,z,marker="*", color="purple")
+ax3.bar(x,z,color="purple")
+
+plt.savefig("subplots2.png",dpi=300)
 plt.show()
 
