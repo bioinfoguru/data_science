345e836ba6383641e8300e64de7e8bc76bcca983
bioinfoguru <support@bioinfo.guru>
2022-06-20 06:24:15 +0530

    Data Science

--- _build/jupyter_execute/Covid19.py
+++ _build/jupyter_execute/Covid19.py
@@ -4,35 +4,58 @@
 # ## Covid19 data analysis
 # Using the data avaiable from Our World in Data, here we'll apply some of the data science concepts to analyze Covid19 data.
 
-# In[10]:
+# In[34]:
 
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
-# In[2]:
+# In[35]:
 
 
 df1 = pd.read_csv("owid-covid-data.csv")
 
 
-# In[3]:
+# In[36]:
 
 
-df1.head()
+df1.shape
+
+
+# In[40]:
+
+
+df1
+
+
+# In[143]:
+
+
+#Countries with maximum number of cases per continent
+df_cont_location = df1.groupby(["continent","location"],as_index=False)["total_cases"].max()
+df_cont_location
+
+
+# In[148]:
+
+
+idx = df1.groupby(['continent'])['total_cases'].transform(max) == df1['total_cases']
+df1[idx]
 
 
 # ### Exercise
 # Find top 10 countries having highest number of total Covid cases till date.
 
-# In[221]:
+# In[69]:
 
 
 top_10 = df1.groupby(["location"], as_index=False)["new_cases"].sum().sort_values(by="new_cases", ascending=False)[8:18]
 
 
-# In[222]:
+# In[96]:
 
 
 top_10
@@ -40,36 +63,48 @@ top_10
 
 # Using `nlargest()` function
 
-# In[223]:
+# In[50]:
 
 
-df1.groupby(["location"],as_index=False)["new_cases"].sum().nlargest(10,"new_cases")
+df_2 = df1.groupby(["location"],as_index=False)["new_cases"].sum()
+
+
+# In[53]:
+
+
+df_2.nlargest(15,"new_cases")
 
 
 # Selecting data for a particular date.
 
-# In[241]:
+# In[54]:
 
 
-df1[df1["date"]=="2022-06-13"].nlargest(10,"total_cases")#[["location","total_cases"]]
+df_date_filter = df1[df1["date"]=="2022-06-13"]
 
 
-# In[33]:
+# In[97]:
 
 
-top_10_r
+df_date_filter.nlargest(10,"total_cases")[["total_cases"]]
 
 
-# In[437]:
+# In[98]:
+
+
+top_10
+
+
+# In[104]:
 
 
 fig, ax = plt.subplots()
-ax.barh(top_10_r.index,top_10_r.values, color="lightblue")
+ax.barh(top_10["location"],top_10["new_cases"], color="lightblue")
 ax.spines[["top","right","left"]].set_visible(False)
 ax.yaxis.set_ticks_position('none')
 a=ax.get_yticklabels()
 ax.grid('on',which='major',axis='x')
-
+ax.invert_yaxis()
 plt.show()
 
 
@@ -107,13 +142,22 @@ ax2.set_title("Log Scale")
 plt.show()
 
 
-# In[298]:
+# In[71]:
 
 
-fig,ax=plt.subplots(figsize=(8,3))
-df1[df1["location"]=="India"].plot(x="date", y="total_cases",ax=ax)
-df1[df1["location"]=="United States"].plot(x="date", y="total_cases",ax=ax)
-df1[df1["location"]=="France"].plot(x="date", y="total_cases",ax=ax)
+fig, ax = plt.subplots(2,1)
+plt(ax=ax[0])
+plt(ax=ax[1])
+
+
+# In[77]:
+
+
+fig,some_axes=plt.subplots(figsize=(8,3))
+
+df1[df1["location"]=="India"].plot(x="date", y="total_cases",ax=some_axes)
+df1[df1["location"]=="United States"].plot(x="date", y="total_cases",ax=some_axes)
+df1[df1["location"]=="France"].plot(x="date", y="total_cases",ax=some_axes)
 plt.legend(["India","United States", "France"])
 plt.ylabel("Total Cases")
 #plt.yscale("log")
@@ -122,7 +166,7 @@ plt.show()
 
 # ### Function to plot total cases
 
-# In[267]:
+# In[79]:
 
 
 def plot_country(*args):
@@ -132,7 +176,7 @@ def plot_country(*args):
     plt.legend(args)
     plt.show()
 
-plot_country("India","United States")
+plot_country("India","United States","France")
 
 
 # In[436]:
@@ -144,43 +188,55 @@ df1[df1["location"]=="India"].plot(x="date", y="new_cases",ax=ax)
 plt.show()
 
 
-# In[349]:
+# In[6]:
 
 
 df_India = df1[df1["location"]=="India"]
 
 
-# In[353]:
+# In[81]:
 
 
-df_India[(df_India["date"] >= '2022-01-01') & (df_India["date"] <= '2022-01-31')].head()
+df_India[(df_India["date"] >= '2022-01-01') & (df_India["date"] <= '2022-01-31')]
 
 
-# In[354]:
+# In[82]:
 
 
 df_India.dtypes
 
 
-# In[355]:
+# In[83]:
 
 
 df_India["date"] = pd.to_datetime(df_India['date'])
 
 
-# In[385]:
+# In[84]:
 
 
 df_India_2022 = df_India[(df_India["date"] >= '2022-01-01')]
 
 
-# In[386]:
+# In[86]:
+
+
+df_India_2022.dtypes
+
+
+# In[87]:
 
 
 df_months = df_India_2022.groupby(df_India_2022['date'].dt.strftime('%B'))["new_cases"].sum().to_frame().reset_index()#.plot(kind="bar")
 
 
-# In[387]:
+# In[88]:
+
+
+df_months
+
+
+# In[89]:
 
 
 print(df_months.dtypes)
@@ -188,7 +244,7 @@ df_months.sort_values(by="date",inplace=True, ignore_index=True)
 df_months
 
 
-# In[388]:
+# In[90]:
 
 
 months = ["January", "February", "March", "April", "May", "June", 
@@ -199,7 +255,7 @@ print(df_months.dtypes)
 df_months
 
 
-# In[401]:
+# In[91]:
 
 
 fig,ax=plt.subplots()
@@ -228,4 +284,65 @@ df_India_2022.dtypes
 ax = df_India_2022.boxplot(column="new_cases",by="date")
 plt.title("")
 plt.show()
+
+
+# In[92]:
+
+
+import seaborn as sns
+
+
+# In[7]:
+
+
+df_India.columns
+
+
+# In[15]:
+
+
+df1.loc[df1['location'].isin(["India","United States"])]
+
+
+# In[18]:
+
+
+sns.jointplot(data=df1.loc[df1['location'].isin(["India","United States"])],              x="new_cases",y="new_deaths", hue="location")
+
+
+# In[93]:
+
+
+new_df = df1.loc[df1['location'].isin(["India","United States"])]
+
+
+# In[94]:
+
+
+new_df
+
+
+# In[95]:
+
+
+sns.jointplot(data=new_df,              x="total_cases",y="total_vaccinations", hue="location")
+
+
+# In[24]:
+
+
+df1.loc[df1['location'].isin(["India","United States"])]
+
+
+# In[29]:
+
+
+sns.pairplot(df1.loc[df1['location'].isin(["India","United States"])]
+             [["location","total_cases","total_deaths","total_vaccinations"]],hue="location")
+
+
+# In[ ]:
+
+
+
 
